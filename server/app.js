a7cf8d36dfcdbffa8718562b83c02047b4f70590
Sam Foster <samueljamesfoster@gmail.com>
2021-03-20 20:27:48 +0000

    Move CORS up middleware

--- server/app.js
+++ server/app.js
@@ -1,51 +1,50 @@
-const createError = require('http-errors');
-const express = require('express');
-const path = require('path');
-const cookieParser = require('cookie-parser');
-const logger = require('morgan');
-const cors = require('cors');
-const helmet = require('helmet');
+const createError = require('http-errors')
+const express = require('express')
+const path = require('path')
+const cookieParser = require('cookie-parser')
+const logger = require('morgan')
+const cors = require('cors')
+const helmet = require('helmet')
 
-const pingRouter = require('./routes/ping');
-const weatherRouter = require('./routes/weather');
+const pingRouter = require('./routes/ping')
+const weatherRouter = require('./routes/weather')
 
-const app = express();
+const app = express()
+app.use(helmet())
+app.use(cors())
 app.use(express.static(path.join(__dirname, '..', 'client', 'build')))
-app.use(helmet());
-app.use(cors());
 
-app.use(logger('dev'));
+app.use(logger('dev'))
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+app.use(cookieParser())
+
+app.use((_req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*")
+  next()
+})
 
 app.use('/ping', pingRouter);
 app.use('/weather', weatherRouter);
 
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  next();
-})
-
-app.get('*', (req, res) => {
+app.get('*', (_req, res) => {
   res.sendFile(path.json(__dirname, '..', 'client', 'build', 'index.html'))
 })
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function(_req, _res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res) {
+app.use((err, req, res) => {
   // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.message = err.message
+  res.locals.error = req.app.get('env') === 'development' ? err : {}
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+  res.status(err.status || 500)
+  res.render('error')
 });
 
 module.exports = app;

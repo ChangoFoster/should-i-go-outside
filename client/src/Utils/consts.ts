f06add639af4e07e279e2c11d26439d864dd6104
Sam Foster <samueljamesfoster@gmail.com>
2022-12-07 21:16:09 +0000

    Update image url

--- client/src/Utils/consts.ts
+++ client/src/Utils/consts.ts
@@ -1,5 +1,5 @@
 const githubProfile = 'https://github.com/ChangoFoster'
-const imgUrl = 'http://www.7timer.info/img/misc/about_two_'
+const imgUrl = 'http://www.7timer.info/img/misc/about_civil_'
 const mentalHealthUrl =
   'https://www.mind.org.uk/information-support/tips-for-everyday-living/physical-activity-and-your-mental-health/about-physical-activity/'
 const weatherProvider = 'https://www.7timer.info'
